6dfd67fdc6ba2fc318f221c11db06b790244079f
AnnieMarkina <anniemurkina@gmail.com>
2020-02-14 08:46:20 +0200

    Updates

--- precache-manifest.af93e43e65bb08a0431adaeb59c9b15a.js
+++ precache-manifest.af93e43e65bb08a0431adaeb59c9b15a.js
@@ -1,7 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "9e34ef571069e429fd98ec596d62a8cc",
+    "revision": "34967977c825bd3fd35cfac924161081",
     "url": "./index.html"
+  },
+  {
+    "revision": "52b01f8e9ad56a0e0f0a",
+    "url": "./static/css/main.36497136.chunk.css"
   },
   {
     "revision": "b25f76c719bdac4a4e1e",
@@ -12,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "./static/js/2.82794d6e.chunk.js.LICENSE"
   },
   {
-    "revision": "6a06f74048c15b338557",
-    "url": "./static/js/main.719dade6.chunk.js"
+    "revision": "52b01f8e9ad56a0e0f0a",
+    "url": "./static/js/main.0eb53e44.chunk.js"
   },
   {
     "revision": "2582523671e821acf2e8",
